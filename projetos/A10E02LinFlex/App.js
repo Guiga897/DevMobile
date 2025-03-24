b904556c61017ee48a508624727b94dcebfd9850
Guiga897 <guifavero10@gmail.com>
2025-03-24 20:47:11 -0300

    2025/03/24

--- projetos/A10E02LinFlex/App.js
+++ projetos/A10E02LinFlex/App.js
@@ -1,20 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View } from "react-native"; 
+import styles from "./styles";
+import Box from "./Box";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( 
+        <View style={styles.container}> {/* Corrigido: >> → } */}
+            <Box>#1</Box>
+            <Box>#2</Box>
+        </View>
+    ); 
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
